fea38a140c6901c2bc6078aa46e69121b942802d
Edet Destiny <edetdestiny700@gmail.com>
2023-07-20 11:13:29 +0100

    fix

--- abiscefoods/settings.py
+++ abiscefoods/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG= True
-ALLOWED_HOSTS = ['46.101.84.142', 'abiscefoods.com']
+ALLOWED_HOSTS = ['46.101.84.142']
 
 
 # Application definition
