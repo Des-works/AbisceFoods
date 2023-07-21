aeecb900b4b955c17ac9a738c6c42ada827daf75
Edet Destiny <edetdestiny700@gmail.com>
2023-07-22 00:02:16 +0100

    IP UPdate

--- abiscefoods/settings.py
+++ abiscefoods/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG= True
-ALLOWED_HOSTS = ['46.101.84.142']
+ALLOWED_HOSTS = ['abiscefoodsltd.com','165.227.226.100']
 
 
 # Application definition
