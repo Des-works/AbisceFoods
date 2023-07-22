c7332ff290d49fea715a8fe8daa75e3a01cb76ba
Edet Destiny <edetdestiny700@gmail.com>
2023-07-22 01:00:05 +0100

    static

--- abiscefoods/settings.py
+++ abiscefoods/settings.py
@@ -121,12 +121,10 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
