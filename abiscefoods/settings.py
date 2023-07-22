063e83c797de446ccc4e3036c7e8d0d062afc078
Edet Destiny <edetdestiny700@gmail.com>
2023-07-22 08:16:52 +0100

    remove the static file dir

--- abiscefoods/settings.py
+++ abiscefoods/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
 
 
 # Default primary key field type
